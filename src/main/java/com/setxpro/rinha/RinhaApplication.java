605781faeb19389d79ff5af0baa8508da7812682
Patrick Anjos <patrickpqdt87289@gmail.com>
2023-10-03 11:44:25 -0300

    feat: initialize

---
+++ src/main/java/com/setxpro/rinha/RinhaApplication.java
@@ -0,0 +1,13 @@
+package com.setxpro.rinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhaApplication.class, args);
+	}
+
+}
